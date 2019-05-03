b1e6f504a901ba79683152ed59054ea97717ea39
Andrea Devola <andreadevola@pc-devola.crs4.it>
2019-05-03 17:33:39 +0200

    Managed the positions comparison between arm in the video and the arm of the statue. Angle positioning between forearm-shoulder, arm-hip assisted by textual suggests

--- degrees2/js/sketch.js
+++ degrees2/js/sketch.js
@@ -1,35 +1,16 @@
 
 let video;
-let poseNet;
 let camPoseNet;
-let poses = [];
-let theta1 = [];
-let theta2 = [];
-let statueAngles = [];
-	
-//let skeleton_vectors = new Array() ;
-/*
-skeleton_vectors['LHS'] = null;
-skeleton_vectors['LES'] = null;
-skeleton_vectors['LEW'] = null;
-skeleton_vectors['LHK'] = null;
-skeleton_vectors['LKA'] = null;
-
-skeleton_vectors['RHS'] = null;
-skeleton_vectors['RES'] = null;
-skeleton_vectors['REW'] = null;
-skeleton_vectors['RHK'] = null;
-skeleton_vectors['RKA'] = null;
-
-skeleton_vectors['LSRS'] = null;
-skeleton_vectors['LHRH'] = null;
-*/
-
 let camPoses = [];
+
+let poses = [];
+let poseNet;
+
 let skeletons = [];
 let skelVecs = [];
 let camSkel = [];
-var s1 = s2 = null;
+let altezze =[]; //eyes, mouth, chin, shoulder, hip, knee, ankle
+//var s1 = s2 = null;
 let count = 0;  
 let camX = 255;
 	  var v1=null;
@@ -40,7 +21,6 @@ var vec;
 
 function setup() {
 	createCanvas(1024, 768);
-  
 	//in order to stop
 	//noLoop();
 
@@ -61,12 +41,12 @@ function setup() {
 
 function draw() {
   //background(150);
-    image(img, 0, 0); // draw a second copy to canvas
-  image(video, camX, 0); // draw the video frame to canvas
-  filter(GRAY);
+	image(img, 0, 0); // draw a second copy to canvas
+	image(video, camX, 0); // draw the video frame to canvas
+	filter(GRAY);
 
-  drawKey();
-  drawSkeleton();
+	drawKey();
+	drawSkeleton();
 
 }
 
@@ -75,9 +55,9 @@ function draw() {
 function imageReady(){
     // set some options
     let options = {
-        imageScaleFactor: 1,
-        minConfidence: 0.1
-    }
+    imageScaleFactor: 1,
+    minConfidence: 0.1    
+}
 
     // assign poseNet
     poseNet = ml5.poseNet(modelReady, options);
@@ -89,7 +69,7 @@ function imageReady(){
 }
 
 function modelReady() {
-  select('#status').html('Imita la posizione del braccio sx (a dx dell\'immagine)');
+  select('#status').html('Let\'s start!');
 	//poseNet.multiPose(img);
 	poseNet.singlePose(img);
 	
@@ -108,8 +88,8 @@ for (let i = 0; i < poses.length; i++) {
 
 	  let vectorAB = "";//[2];
 
-	  count++;
-	  console.log("Count value:", count);
+	  //count++;
+	  //console.log("Count value:", count);
 
 	  let currentA = partA["part"];
 	  let currentB = partB["part"];
@@ -119,74 +99,28 @@ for (let i = 0; i < poses.length; i++) {
 
 	  vectorAB=createVector(partB.position.x - partA.position.x, partB.position.y - partA.position.y);
 	  vectorAB.name = currentA+currentB;
-	 
-	  //skeleton_vectors['LES'] = null;
-	  //skeleton_vectors['LEW'] = null;
 	  
-	  if ((s1==null)&& vectorAB.name == "leftElbowleftShoulder")
-	  {	s1 = vectorAB; console.log("S1: ", s1);}
-	  if ((s2==null) && vectorAB.name == "leftElbowleftWrist")
-	  {	s2 = vectorAB; console.log("S2: ", s2);}
-
-	  
-	  skelVecs[count-1] = vectorAB;
-	  
-	  if( (v1 == null) || (check ==0))
-	  {
-	    console.log("V1 = NULL OPPURE CHECK == 0");
-	    console.log("Quindi valorizzo V1 con ab, e segno CHECK = 1.");	    
-	  	v1=vectorAB;
-	    console.log("** v1 valorizzato: ", v1)
-
-	  	check = 1;
-	  	
-	  }
-	  else
-	  {
-	    console.log("Se V1 diverso da null e CHECK diverso da 0");
-	    console.log("Il count sara' cresciuto; Valorizzo v2 con ab, pongo CHECK = 0.");
-
-	  	v2=vectorAB;
-		console.log("** v2 valorizzato: ", v2)
-	  	
-	  	check = 0;
-
-	  }
-	  if( (v1 != null && v2!=null) && check == 0)
-	  {
-	    console.log("A questo punto dovrei trovare v1 e v2 diversi da null, e check = 0:");
-	    console.log("Quindi stampo i due vettori, calcolo e stampo l'angolo tra i due.");
-	    
-
-	  	//calcolo l'angolo tra i due vettori
-	  	console.log("** v1: ", v1)
-	  	console.log("** v2: ", v2)
-	  	console.log("************************** vettore ************************** ", vettore);
-
-
-	  	let angle = degrees(v2.angleBetween(v1));
-	  	theta1[v1.name+v2.name]=angle;
-
-
-	  	console.log("** angle between v2, v1: ", angle);
-
-
-	  }
-
+	  skelVecs[currentA+currentB] = vectorAB;
 
 	  if((partA["part"] == "leftHip") && (partB["part"] == "rightHip"))
 	  { 	
-	   console.log("*********************************   daccapo");
+	   console.log("*********************************   fine");
 	   count = 0;
 	  }
+	  
+	  
+	  
 	}
+	
+	
   }
+  /*
   	  if((s1!=null)&&(s2!=null))
 	  {	
 	  	statueAngles[s1.name+s2.name]= degrees(s2.angleBetween(s1));
 	    console.log("Statue angle left ELBOWSHOULDER left ELBOWWRIST: ", statueAngles[s1.name+s2.name]);
 	  }
-  
+  */
 	/////
 	showSkelVecs();
 
@@ -205,8 +139,8 @@ function showSkelVecs()
 function getCamPoses(results) {
 	camPoses = results;
 	let vectorCD = "";//[2];
-	let c1 = c2 = null;
-
+	let LES = LEW = LHS = null;
+	
 	//console.log("POSES: ",camPoses);
 	/*
 skeleton_vectors['LHS'] = null;
@@ -231,9 +165,23 @@ skeleton_vectors['LHRH'] = null;
 	 //skelVecs // camPoses // let skelVecs = [];
 
 	//let count = 0;
+	let LESLEW = degrees( skelVecs["leftElbowleftWrist"].angleBetween(skelVecs["leftElbowleftShoulder"]));
+	let LHSLES = degrees( skelVecs["leftElbowleftShoulder"].angleBetween(skelVecs["leftHipleftShoulder"]));
+	
+	
+	let point;
+	let lWristHeight, lShoulderHeight;
+	/*
+	point = poses[0].pose.keypoints[0];
+	console.log("THE POINT IS: ", point.position.y);
+
+	*/
 	
 	for (let i = 0; i < camPoses.length; i++) {
-	
+		
+		//if(camPoses[i].pose.keypoints[0].part == "nose")
+		//	console.log("abbiamo il naso all'altezza di: ", camPoses[i].pose.keypoints[0].position.y);
+			
     	for (let j = 0; j < camPoses[i].skeleton.length; j++) {
     	
       		let partA = camPoses[i].skeleton[j][0];
@@ -241,7 +189,7 @@ skeleton_vectors['LHRH'] = null;
 
 	 
       		let keypoint = camPoses[i].pose.keypoints[j];
-      		
+
       		if (keypoint.score > 0.2)
       		{
       		 	vectorCD=createVector(partB.position.x - partA.position.x, partB.position.y - partA.position.y);
@@ -251,26 +199,89 @@ skeleton_vectors['LHRH'] = null;
 	  			//camSkel[count].name = partA["part"]+partB["part"];
 	  			//count++;
 	  		}
-			if((c1==null)&& vectorCD.name == "leftElbowleftShoulder")
-				{	c1 = vectorCD; }
-			if ((c2==null) && vectorCD.name == "leftElbowleftWrist")
-				{	c2 = vectorCD; console.log("C1: ", c1); console.log("C2: ", c2);}
+			if((LES == null)&& vectorCD.name == "leftElbowleftShoulder")
+				{	LES = vectorCD; }
+			if ((LEW == null) && vectorCD.name == "leftElbowleftWrist")
+				{	LEW = vectorCD; console.log("LES: ", LES); console.log("LEW: ", LEW);}
+			if ((LHS == null) && vectorCD.name == "leftHipleftShoulder")
+				{	LHS = vectorCD; console.log("LHS: ", LHS); console.log("LHS: ", LHS);}
 	  		
-  	  		if((c1!=null)&&(c2!=null))
+	  		
+	  		      		/*console.log("*****Keypoint cam-part: ", camPoses[i].pose.keypoints[9] );
+      		
+      		if(keypoint.part == "leftWrist") 
+      		{	lWristHeight = keypoint.position.y;	console.log("******wrist position NAME?: ", keypoint.part);}
+      		if(keypoint.part == "leftShoulder") 
+      			lShoulderHeight = keypoint.position.y;	
+      		
+      		//console.log("******wrist position NAME?: ", keypoint.part);
+      		//console.log("******wrist position from CAM: ", lWristHeight);
+      		*/
+	  		
+	  		//LES^LEW
+  	  		if((LES!=null)&&(LEW!=null))
 	  		{	
-	  			let camAngle = Math.floor ( degrees(c2.angleBetween(c1)) );// Math.floor (n);
-	  			if(camAngle < Math.floor (statueAngles[s1.name+s2.name]))
-	  				{print("Angolo inferiore!"); select('#status').html('Imita la posizione del braccio sx (a dx dell\'immagine): angolo inferiore.');}
-	  			else if(camAngle > Math.floor (statueAngles[s1.name+s2.name]))
-	  				{print("Angolo maggiore!"); select('#status').html('Imita la posizione del braccio sx (a dx dell\'immagine): angolo maggiore.');}
-	  			else 
-	  				{ print("Angolo uguale!"); select('#status').html('Imita la posizione del braccio sx (a dx dell\'immagine): angolo uguale!!.');}
-	    		//console.log("Statue angle left ELBOWSHOULDER left ELBOWWRIST: ", statueAngles[s1.name+s2.name]);
+	  			let camAngle = Math.floor ( degrees(LEW.angleBetween(LES)) );// Math.floor (n);
+	  			//console.log("Angolo cam: ", camAngle);
+	  	    			    		
+	    		
+	    		/* //ALTEZZA POLSO RISPETTO AL CANVAS - DA RIVEDERE IN FUNZIONE DI: 
+	    		// - altezze relative a parti del corpo invece che il canvas
+	    		// - altezze relative a asse y invertita	    		
+	    		if( camPoses[i].pose.keypoints[9].position.y > poses[0].pose.keypoints[9].position.y+30 )
+	    			{console.log("Devi abbassare ancora il polso..");  select('#lWristHeight').html('devi decisamente abbassarlo.');}
+	    		else if(camPoses[i].pose.keypoints[9].position.y > poses[0].pose.keypoints[9].position.y+15 )
+	    			{print("Devi abbassare ancora il polso, ma ci siamo quasi!"); select('#lWristHeight').html('Devi abbassare ancora il polso, ma ci siamo quasi!');}
+	    		else if(camPoses[i].pose.keypoints[9].position.y < poses[0].pose.keypoints[9].position.y-30 )
+	    			{print("Devi sollevare ancora il polso.."); select('#lWristHeight').html('devi decisamente sollevarlo.');}
+	    		else if(camPoses[i].pose.keypoints[9].position.y < poses[0].pose.keypoints[9].position.y-15 )
+	    			{print("Devi sollevare ancora il polso, ma ci siamo quasi!"); select('#lWristHeight').html('Devi sollevare ancora il polso, ma ci siamo quasi!');}		
+	    		else {print("Altezza del polso ok!"); select('#lWristHeight').html('Così va bene!.');}
+				*/	    				    			
+	    		
+	    		if(camAngle < Math.floor(LESLEW)-20)//statueAngles[s1.name+s2.name])-1)
+	  				{print("Angolo LESLEW inferiore!"); select('#LESLEW').html('decisamente inferiore.');}
+	  			else if(camAngle < Math.floor(LESLEW)-10)//statueAngles[s1.name+s2.name])-1)
+	  				{print("Angolo LESLEW inferiore!"); select('#LESLEW').html('ancora inferiore, ma ci siamo quasi.');}			  			
+	  			
+	  			
+	  			else if(camAngle > Math.floor(LESLEW)+20)
+	  				{print("Angolo LESLEW maggiore!"); select('#LESLEW').html('decisamente maggiore.');}
+	  			else if(camAngle > Math.floor(LESLEW)+10)
+	  				{print("Angolo LESLEW maggiore!"); select('#LESLEW').html('ancora maggiore, ma ci siamo quasi..');}	  	
+	  				
+	  			else if( (camAngle > Math.floor(LESLEW)-5) && (camAngle < Math.floor(LESLEW)+5) )//statueAngles[s1.name+s2.name])-1)
+	  				{ print("Angolo LESLEW uguale!"); select('#LESLEW').html(' uguale!!.');}	  							
+	  		 
+	  				
 	  		}
+	  		
+	  		//LHS^LES
+  	  		if((LHS != null)&&(LES != null))
+	  		{	
+	  			let camAngle = Math.floor ( degrees(LES.angleBetween(LHS)) );// Math.floor (n);
+	  			//console.log("Angolo cam: ", camAngle);
+	  			
+	  			
+	  			console.log("Angolo statue: ", Math.floor (LHSLES)); //statueAngles[s1.name+s2.name]));
+	  			if(camAngle < Math.floor(LHSLES)-20)//statueAngles[s1.name+s2.name])-1)
+	  				{print("Angolo inferiore!"); select('#LHSLES').html('decisamente inferiore.');}
+	  			else if(camAngle < Math.floor(LHSLES)-10)//statueAngles[s1.name+s2.name])-1)
+	  				{print("Angolo inferiore!"); select('#LHSLES').html('ancora inferiore, ma ci siamo quasi.');}
+	  					  				
+	  			else if(camAngle > Math.floor(LHSLES)+20)//statueAngles[s1.name+s2.name])+1)
+	  				{print("Angolo maggiore!"); select('#LHSLES').html('decisamente maggiore.');}
+	  			else if(camAngle > Math.floor(LHSLES)+10)//statueAngles[s1.name+s2.name])+1)
+	  				{print("Angolo maggiore!"); select('#LHSLES').html('ancora maggiore, ma ci siamo quasi...');}	  				
+	  			else if( (camAngle > Math.floor(LHSLES)-5) && (camAngle < Math.floor(LHSLES)+5) )
+	  				{ print("Angolo uguale!"); select('#LHSLES').html(' uguale!!.');}
+	    		//console.log("Statue angle left ELBOWSHOULDER left ELBOWWRIST: ", statueAngles[s1.name+s2.name]);
+	  		}	  		
 			//console.log("PART-A from showSkelVecs: ", partA["part"]);
 			//leftHipleftShoulderleftElbowleftShoulder
 			
       	}
+      	//console.log("**********Vettore di keypoints: ",camPoses[i].pose.keypoints);
       }
      //console.log("vettori da cam: ",camSkel);
      
@@ -342,6 +353,8 @@ skeleton_vectors['LHRH'] = null;
 function drawKey()
 {
   //
+  let altezze =[]; //eyes, mouth, chin, shoulder, hip, knee, ankle
+
   for (let i = 0; i < poses.length; i++) {
     for (let j = 0; j < poses[i].pose.keypoints.length; j++) {
       let keypoint = poses[i].pose.keypoints[j];
